138d12fda7befd50829fd880fa96ad30f0ca73e2
Yuchen Zhong <yuchen@hopper.com>
2019-11-17 22:24:58 -0500

    day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,15 +1,21 @@
 use std::fs;
 
 pub fn fn day1_part1() {
-    let filename = "2018:day:1:input.txt";
-    let contents: String =
+    let filenames = [
+        // "2018:day:1:input.txt",
+        "2018:day:1:input_test1.txt"
+    ]
+
+    for filename in filenames {
+        let contents: String =
         fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let mut total = 0;
-    for number_string in contents.split("\n") {
-        if number_string.len() != 0 {
-            println!("Parsing number {}", number_string);
-            total += number_string.parse::<i32>().unwrap();
+        let mut total = 0;
+        for number_string in contents.split("\n") {
+            if number_string.len() != 0 {
+                println!("Parsing number {}", number_string);
+                total += number_string.parse::<i32>().unwrap();
+            }
         }
+        println!("{}", total)
     }
-    println!("{}", total)
 }
--- src/day1_part2.rs
+++ src/day1_part2.rs
@@ -2,26 +2,44 @@ use std::collections::HashSet;
 use std::fs;
 
 pub fn day1_part2() {
-    let filename = "2018:day:1:input.txt";
-    let contents: String =
-        fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let mut total = 0;
-    let mut set: HashSet<i32> = HashSet::new();
-    for number_string in contents.split("\n") {
-        if number_string.len() != 0 {
-            println!(
-                "Parsing number {}, current total is {}",
-                number_string, total
-            );
-            total += number_string.parse::<i32>().unwrap();
+    let filenames = [
+        "2018:day:1:input.txt",
+        // "2018:day:1:input_test1.txt",
+        // "2018:day:1:input_test2.txt",
+    ];
 
-            if set.contains(&total) {
-                println!("Already seen this frequency: {}", total);
-                break;
-            } else {
-                println!("New total {}", total);
-                set.insert(total);
+    for filename in filenames.iter() {
+        let contents: String =
+            fs::read_to_string(filename).expect("Something went wrong reading the file");
+        let mut total = 0;
+        let mut seens: HashSet<i32> = HashSet::new();
+        seens.insert(0);
+        loop {
+            let mut found = false;
+            for number_string in contents.split("\n") {
+                if number_string.len() != 0 {
+                    // println!(
+                    //     "Parsing number {}, current total is {}",
+                    //     number_string, total
+                    // );
+                    total += number_string.parse::<i32>().unwrap();
+
+                    if seens.contains(&total) {
+                        println!("Already seen this frequency: {}", total);
+                        found = true;
+                        break;
+                    } else {
+                        println!("New total {}", total);
+                        seens.insert(total);
+                    }
+                }
             }
+
+            if found {
+                break;
+            }
+
+            println!("Loop finished. Restart");
         }
     }
 }
