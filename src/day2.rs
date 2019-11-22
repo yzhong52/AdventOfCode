445421e1a784da003e5f5000f84fc049a8a44c01
Yuchen Zhong <yzhong.cs@gmail.com>
2019-11-22 08:26:38 -0500

    day 2 part 1 finished

--- src/day2.rs
+++ src/day2.rs
@@ -3,47 +3,22 @@ use super::helpers::Answer;
 use std::collections::HashMap;
 
 pub fn day2(input: Input<Vec<String>>) -> Answer<i32> {
-    let mut twos = 0;
-    let mut threes = 0;
+    let mut two_count = 0;
+    let mut three_count = 0;
 
     for st in input.data {
-        println!("{:?}", st);
         let mut map: HashMap<char, i32> = HashMap::new();
-        println!("{:?}", map);
 
         for ch in st.chars() {
             let count = map.get(&ch).unwrap_or(&0) + 1;
             map.insert(ch, count);
         }
 
-        let mut has_two = false;
-        let mut has_three = false;
+        let has_two = map.values().any(|x| *x == 2);
+        let has_three = map.values().any(|x| *x == 3);
 
-        for count in map.values() {
-            match count {
-                2 => {
-                    has_two = true;
-                }
-                3 => {
-                    has_three = true;
-                }
-                _ => { },
-            }
-        }
-
-        if has_two {
-            twos += 1
-        }
-
-        if has_three {
-            threes += 1
-        }
-
-        println!("{:?} {} {}", &map, has_two, has_three);
-        println!("==> {}x{}\n\n\n", twos, threes);
+        two_count += has_two as i32;
+        three_count += has_three as i32;
     }
-
-    println!("{:?}", twos * threes);
-
-    return Answer { question: input.question, result: twos * threes };
+    return Answer { question: input.question, result: two_count * three_count };
 }
