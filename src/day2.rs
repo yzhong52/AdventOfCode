06d0b5f41695b577698f891144319855a646c09b
Yuchen Zhong <yzhong.cs@gmail.com>
2019-11-22 08:11:11 -0500

    day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,7 +1,49 @@
 use super::helpers::Input;
 use super::helpers::Answer;
+use std::collections::HashMap;
 
 pub fn day2(input: Input<Vec<String>>) -> Answer<i32> {
-    // TODO: Yuchen -
-    return Answer { question: input.question, result: 2 };
+    let mut twos = 0;
+    let mut threes = 0;
+
+    for st in input.data {
+        println!("{:?}", st);
+        let mut map: HashMap<char, i32> = HashMap::new();
+        println!("{:?}", map);
+
+        for ch in st.chars() {
+            let count = map.get(&ch).unwrap_or(&0) + 1;
+            map.insert(ch, count);
+        }
+
+        let mut has_two = false;
+        let mut has_three = false;
+
+        for count in map.values() {
+            match count {
+                2 => {
+                    has_two = true;
+                }
+                3 => {
+                    has_three = true;
+                }
+                _ => { },
+            }
+        }
+
+        if has_two {
+            twos += 1
+        }
+
+        if has_three {
+            threes += 1
+        }
+
+        println!("{:?} {} {}", &map, has_two, has_three);
+        println!("==> {}x{}\n\n\n", twos, threes);
+    }
+
+    println!("{:?}", twos * threes);
+
+    return Answer { question: input.question, result: twos * threes };
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,25 @@ mod helpers;
 
 use helpers::Question;
 use helpers::read_ints;
+use helpers::Input;
 use crate::helpers::read_strings;
 
 fn main() {
     day1::part1(read_ints(Question { year: 2018, day: 1 })).save_as("part1");
     day1::part2(read_ints(Question { year: 2018, day: 1 })).save_as("part2");
-
     day2::day2(read_strings(Question { year: 2018, day: 2 })).save();
+
+    let data: Vec<String> = [
+        "abcdef",
+        "bababc",
+        "abbcde",
+        "abcccd",
+        "aabcdd",
+        "abcdee",
+        "ababab"
+    ].to_vec().iter().map(|x| x.to_string()).collect();
+
+    let question = Question { year: 0, day: 0 };
+    let input: Input<Vec<String>> = Input { question, data };
+    day2::day2(input);
 }
