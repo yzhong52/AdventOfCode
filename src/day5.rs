2056620e410bfab3fc12be1c291d6581a85b20d6
Yuchen Zhong <yuchen@hopper.com>
2019-11-25 20:38:42 -0500

    day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,14 +1,15 @@
 use super::helpers::Input;
 use super::helpers::Answer;
+use std::cmp::min;
 
 fn diff(c1: &char, c2: &char) -> i32 {
     return i32::abs(*c1 as i32 - *c2 as i32);
 }
 
-pub fn part1(input: Input<String>) -> Answer<usize> {
+pub fn remains(input: String) -> Vec<char> {
     let mut stack: Vec<char> = Vec::new();
 
-    for x in input.data.chars() {
+    for x in input.chars() {
         match stack.last() {
             Some(c) => {
                 if diff(&x, c) == diff(&'A', &'a') {
@@ -25,6 +26,29 @@ pub fn part1(input: Input<String>) -> Answer<usize> {
             }
         }
     }
+    return stack;
+}
 
+pub fn part1(input: Input<String>) -> Answer<usize> {
+    let mut stack: Vec<char> = remains(input.data);
     return Answer { question: input.question, result: stack.len() };
+}
+
+pub fn part2(input: Input<String>) -> Answer<usize> {
+    let part1_result: Vec<char> = remains(input.data);
+
+    let mut result = part1_result.len();
+
+    for d in 0..26 {
+        let ignore_c1: char = ('a' as i32 + d) as u8 as char;
+        let ignore_c2: char = ('A' as i32 + d) as u8 as char;
+
+        let filtered_input: String = part1_result.iter()
+            .filter(|x| **x != ignore_c1 && **x != ignore_c2)
+            .collect();
+
+        result = min(result, remains(filtered_input).len());
+    }
+
+    return Answer { question: input.question, result };
 }
--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,7 @@ fn main() {
     day1::part2(read_ints(Question { year: 2018, day: 1 })).save_as("part2");
     day2::day2(read_strings(Question { year: 2018, day: 2 })).save();
     day4::part1(read_strings(Question { year: 2018, day: 4 })).save_as("part1");
+
     day5::part1(read_string(Question { year: 2018, day: 5 })).save_as("part1");
+    day5::part2(read_string(Question { year: 2018, day: 5 })).save_as("part2");
 }
