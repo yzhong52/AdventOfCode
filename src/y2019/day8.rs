1716be33d64fde3341297d86df0d0bf44a85c1cd
Yuchen Zhong <yzhong.cs@gmail.com>
2019-12-08 16:12:16 -0500

    hmm ... not right?

--- src/y2019/day8.rs
+++ src/y2019/day8.rs
@@ -15,16 +15,39 @@ pub fn part1(input: Input<String>) -> Answer<usize> {
         let zeros = layer.chars().filter(|x| x == &'0').count();
         let ones = layer.chars().filter(|x| x == &'1').count();
         let twos = layer.chars().filter(|x| x == &'2').count();
-        println!("{} {} {}", zeros, ones, twos);
 
         if minimum_number_of_zeros > zeros {
             minimum_number_of_zeros = zeros;
             result = ones * twos;
         }
     }
-    return Answer { question: input.question, result: result };
+    return Answer { question: input.question, result };
 }
 
-pub fn part2(input: Input<String>) -> Answer<usize> {
-    return Answer { question: input.question, result: 0 };
+pub fn part2(input: Input<String>) -> Answer<String> {
+    let pixels_count_per_layer = 25 * 6;
+    let num_of_layers = input.data.len() / pixels_count_per_layer;
+
+    let mut visible_layer: Vec<char> = vec!['2'; pixels_count_per_layer];
+
+    for layer_index in 0..num_of_layers {
+        let layer: String = input.data.chars()
+            .skip(layer_index * pixels_count_per_layer)
+            .take(pixels_count_per_layer)
+            .collect();
+
+        for (i, c) in layer.chars().into_iter().enumerate() {
+            if visible_layer[i] == '2' && c != '2' {
+                visible_layer[i] = c;
+            }
+        }
+
+        for c in &visible_layer {
+            print!("{}", c);
+        }
+        println!()
+    }
+
+    let result: String = visible_layer.into_iter().collect();
+    return Answer { question: input.question, result };
 }
