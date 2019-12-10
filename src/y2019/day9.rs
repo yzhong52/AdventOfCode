91b9b0a9cde9a585061685c607ea0244662783cb
Yuchen Zhong <yzhong.cs@gmail.com>
2019-12-09 23:48:48 -0500

    day 9

--- src/y2019/day9.rs
+++ src/y2019/day9.rs
@@ -14,7 +14,7 @@ const OPERATION_JUMP_IF_TRUE_5: i128 = 5;
 const OPERATION_JUMP_IF_FALSE_6: i128 = 6;
 const OPERATION_LESS_THAN_7: i128 = 7;
 const OPERATION_EQUAL_8: i128 = 8;
-const OPERATION_RELATIVE_BASE_OFFSET: i128 = 9;
+const OPERATION_RELATIVE_BASE_OFFSET_9: i128 = 9;
 
 pub enum SuperIntCodeResult {
     Output(i128),
@@ -28,70 +28,88 @@ pub struct SuperIntCodeComputer {
     pub input_queue: VecDeque<i128>,
     pub relative_base: usize,
 
-    pub external_numbers: HashMap<i128, i128>
+    pub external_numbers: HashMap<usize, i128>,
 }
 
 impl SuperIntCodeComputer {
     fn parse_number(&self, mode: i128, relative_base: usize) -> i128 {
         match mode {
-            POSITION_MODE => self.numbers[self.numbers[self.index] as usize],
-            IMMEDIATE_MODE => self.numbers[self.index as usize],
-            RELATIVE_MODE => self.numbers[(self.numbers[self.index] + relative_base as i128) as usize],
+            POSITION_MODE => self.read(self.read(self.index) as usize),
+            IMMEDIATE_MODE => self.read(self.index as usize),
+            RELATIVE_MODE => self.read((self.read(self.index) + relative_base as i128) as usize),
             i => unimplemented!("{}", i),
         }
     }
 
+    fn read(&self, index: usize) -> i128 {
+        let default_value = 0 as i128;
+        if index < self.numbers.len() {
+            self.numbers[index]
+        } else {
+            *self.external_numbers.get(&index).unwrap_or_else({ || &default_value })
+        }
+    }
+
+    fn save(&mut self, index: usize, value: i128) {
+        if index < self.numbers.len() {
+            self.numbers[index] = value;
+        } else {
+            self.external_numbers.insert(index, value);
+        }
+    }
+
     pub fn run(&mut self) -> SuperIntCodeResult {
-        while self.numbers[self.index] != 99 {
-            let current_instruction = self.numbers[self.index];
+        while self.read(self.index) != 99 {
+            let current_instruction = self.read(self.index);
             let operation_code = current_instruction % 100;
             let mode1 = current_instruction / 100 % 10;
             let mode2 = current_instruction / 1000 % 10;
-
             self.index += 1;
 
             match operation_code {
                 OPERATION_ADDITION_1 | OPERATION_MULTIPLICATION_2 | OPERATION_LESS_THAN_7 | OPERATION_EQUAL_8 => {
                     let parameter1 = self.parse_number(mode1, self.relative_base);
                     self.index += 1;
-                    let parameter2 = self.parse_number( mode2, self.relative_base);
+                    let parameter2 = self.parse_number(mode2, self.relative_base);
                     self.index += 1;
-                    let parameter3 = self.numbers[self.index] as usize;
+                    let parameter3 = self.read(self.index) as usize;
                     self.index += 1;
-                    self.numbers[parameter3] = match operation_code {
+                    let value = match operation_code {
                         OPERATION_ADDITION_1 => parameter1 + parameter2,
                         OPERATION_MULTIPLICATION_2 => parameter1 * parameter2,
                         OPERATION_LESS_THAN_7 => (parameter1 < parameter2) as i128,
                         OPERATION_EQUAL_8 => (parameter1 == parameter2) as i128,
                         i => unimplemented!("{}", i),
                     };
+                    self.save(parameter3, value);
                 }
                 OPERATION_INPUT_3 => {
-                    let position = self.numbers[self.index] as usize;
-                    self.numbers[position] = self.input_queue.pop_front().unwrap();
+                    let position = self.read(self.index) as usize;
+                    let value = self.input_queue.pop_front().unwrap();
+                    self.save(position, value);
                     self.index += 1;
                 }
                 OPERATION_OUTPUT_4 => {
-                    let output_number = self.parse_number( mode1, self.relative_base);
+                    let output_number = self.parse_number(mode1, self.relative_base);
                     self.index += 1;
                     return SuperIntCodeResult::Output(output_number);
                 }
                 OPERATION_JUMP_IF_TRUE_5 | OPERATION_JUMP_IF_FALSE_6 => {
-                    let parameter1 = self.parse_number( mode1, self.relative_base);
+                    let parameter1 = self.parse_number(mode1, self.relative_base);
                     self.index += 1;
-                    let parameter2 = self.parse_number( mode2, self.relative_base);
+                    let parameter2 = self.parse_number(mode2, self.relative_base);
                     self.index += 1;
 
                     self.index = match operation_code {
                         OPERATION_JUMP_IF_TRUE_5 if parameter1 != 0 => parameter2 as usize,
                         OPERATION_JUMP_IF_FALSE_6 if parameter1 == 0 => parameter2 as usize,
                         _ => self.index,
-                    }
+                    };
                 }
-                OPERATION_RELATIVE_BASE_OFFSET => {
-                    let parameter1 = self.parse_number( mode1, self.relative_base);
+                OPERATION_RELATIVE_BASE_OFFSET_9 => {
+                    let parameter1 = self.read(self.index);
                     self.index += 1;
-                    self.relative_base += (self.relative_base as i128 + parameter1) as usize;
+                    self.relative_base = (self.relative_base as i128 + parameter1) as usize;
                 }
                 i => unimplemented!("{}", i),
             };
@@ -107,26 +125,29 @@ pub fn run_till_halt(values: &Vec<i128>, inputs: Vec<i128>) -> i128 {
         index: 0,
         input_queue: inputs.into_iter().collect(),
         relative_base: 0,
-        external_numbers: HashMap::new()
+        external_numbers: HashMap::new(),
     };
     let mut final_output = 0;
     loop {
         match computer.run() {
-            SuperIntCodeResult::Output(value) => final_output = value,
+            SuperIntCodeResult::Output(value) => {
+                println!("Output value {}", value);
+                final_output = value
+            }
             SuperIntCodeResult::Halted => break
         }
     }
     final_output
 }
 
-fn run_day5(values: &Vec<i128>, input_value: i128) -> i128 {
+fn run_day9(values: &Vec<i128>, input_value: i128) -> i128 {
     run_till_halt(values, vec![input_value])
 }
 
 pub fn part1(input: Input<Vec<i128>>) -> Answer<i128> {
-    Answer { question: input.question, result: run_day5(&input.data, 1) }
+    Answer { question: input.question, result: run_day9(&input.data, 1) }
 }
 
 pub fn part2(input: Input<Vec<i128>>) -> Answer<i128> {
-    Answer { question: input.question, result: run_day5(&input.data, 5) }
+    Answer { question: input.question, result: 0 }
 }
