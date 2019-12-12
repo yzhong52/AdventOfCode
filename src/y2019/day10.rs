f00ce6b97009279c6f470b066b0de72bf7883f3d
Yuchen Zhong <yzhong.cs@gmail.com>
2019-12-12 08:03:04 -0500

    day 10

--- src/y2019/day10.rs
+++ src/y2019/day10.rs
@@ -2,23 +2,26 @@ use super::super::helpers::parser::*;
 use std::collections::HashSet;
 
 fn count_visible(data: &Vec<Vec<char>>, x: usize, y: usize) -> usize {
-    let mut set: HashSet<(bool, bool, i32)> = HashSet::new();
+    let mut set: HashSet<(i32, bool)> = HashSet::new();
     for (i, row) in data.iter().enumerate() {
         for (j, c) in row.iter().enumerate() {
-            if c == &'#' && !(i == x && j == y) {
+            if c == &'#' {
                 let diff_x = x as i32 - i as i32;
                 let diff_y = y as i32 - j as i32;
 
-                let value: i32;
-                if diff_x != 0 {
-                    value = ((diff_y as f32 / diff_x as f32) * 10000.0) as i32;
-                } else {
-                    // value = 0;
-                    value = ((diff_y as f32 / diff_x as f32) * 10000.0) as i32;
-//                    println!("{} {} {}", diff_y as f32, diff_x as f32, diff_y as f32 / diff_x as f32); // value = ((diff_y as f32 / diff_x as f32) * 10000.0) as i32;
+                match (diff_x, diff_y) {
+                    (0, 0) => (),
+                    (0, dy) if dy < 0 => {
+                        set.insert((std::i32::MIN, false));
+                    }
+                    (0, dy) if dy > 0 => {
+                        set.insert((std::i32::MAX, false));
+                    }
+                    (dx, dy) => {
+                        let value: i32 = ((dy as f32 / dx as f32) * 10000.0) as i32;
+                        set.insert((value, dx > 0));
+                    }
                 }
-
-                set.insert((diff_x > 0, diff_y > 0, value));
             }
         }
     }
