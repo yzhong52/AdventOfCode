c836f393809452d92f74ccad1d7f5b6f4fe088fb
Yuchen Zhong <yzhong.cs@gmail.com>
2019-12-14 08:44:49 -0500

    This is the right answer What?!

--- src/y2019/day12.rs
+++ src/y2019/day12.rs
@@ -1,6 +1,5 @@
 use super::super::helpers::parser::*;
 use super::super::helpers::models::*;
-use std::collections::HashMap;
 
 pub fn parse_points(data: &Vec<String>) -> Vec<Point3D> {
     let mut points: Vec<Point3D> = vec![];
@@ -29,50 +28,103 @@ fn adjust(left: i32, right: i32) -> i32 {
     }
 }
 
+fn update_velocities(points: &Vec<Point3D>, velocities: &mut Vec<Point3D>) {
+    for i in 0..points.len() {
+        for j in i + 1..points.len() {
+            velocities[i].x += adjust(points[j].x, points[i].x);
+            velocities[i].y += adjust(points[j].y, points[i].y);
+            velocities[i].z += adjust(points[j].z, points[i].z);
+
+            velocities[j].x += adjust(points[i].x, points[j].x);
+            velocities[j].y += adjust(points[i].y, points[j].y);
+            velocities[j].z += adjust(points[i].z, points[j].z);
+        }
+    }
+}
+
+fn update_positions(points: &mut Vec<Point3D>, velocities: &Vec<Point3D>) {
+    for i in 0..points.len() {
+        points[i].x += velocities[i].x;
+        points[i].y += velocities[i].y;
+        points[i].z += velocities[i].z;
+    }
+}
+
 pub fn part1(input: Input<Vec<String>>) -> Answer<i32> {
     let mut points: Vec<Point3D> = parse_points(&input.data);
     let mut velocities: Vec<Point3D> = vec![Point3D::origin(); points.len()];
 
     let mut total_energy: i32 = 0;
     for _ in 0..1000 {
-        let mut next_velocities = velocities.clone();
-
-        for i in 0..points.len() {
-            for j in i + 1..points.len() {
-                next_velocities[i].x += adjust(points[j].x, points[i].x);
-                next_velocities[i].y += adjust(points[j].y, points[i].y);
-                next_velocities[i].z += adjust(points[j].z, points[i].z);
-
-                next_velocities[j].x += adjust(points[i].x, points[j].x);
-                next_velocities[j].y += adjust(points[i].y, points[j].y);
-                next_velocities[j].z += adjust(points[i].z, points[j].z);
-            }
-        }
-
-        for i in 0 .. points.len() {
-            points[i].x += next_velocities[i].x;
-            points[i].y += next_velocities[i].y;
-            points[i].z += next_velocities[i].z;
-
-            println!("{:?} {:?}", points[i], velocities[i]);
-        }
-
-        velocities = next_velocities;
+        update_velocities(&points, velocities.as_mut());
+        update_positions(points.as_mut(), &velocities);
 
         total_energy = 0;
-        for i in 0 .. points.len() {
+        for i in 0..points.len() {
             let kinetic_energy = velocities[i].x.abs() + velocities[i].y.abs() + velocities[i].z.abs();
             let potential_energy = points[i].x.abs() + points[i].y.abs() + points[i].z.abs();
             total_energy += kinetic_energy * potential_energy
         }
-        println!(" - - - - - {} ", total_energy);
     }
     return Answer { question: input.question, result: total_energy };
 }
 
 
 pub fn part2(input: Input<Vec<String>>) -> Answer<usize> {
-    return Answer { question: input.question, result: 0 };
+    let initial_points: Vec<Point3D> = parse_points(&input.data);
+
+    fn x(p: &Point3D) -> i32 { p.x }
+    fn y(p: &Point3D) -> i32 { p.y }
+    fn z(p: &Point3D) -> i32 { p.z }
+
+    let dimensions: [fn(&Point3D) -> i32; 3] = [x, y, z];
+
+    let results: Vec<usize> = dimensions.iter().map(|dimension_function| {
+        // TODO: Should this start with 0?
+        let mut index: usize = 1;
+        let mut points: Vec<Point3D> = initial_points.clone();
+        let mut velocities: Vec<Point3D> = vec![Point3D::origin(); points.len()];
+
+        loop {
+            update_velocities(&points, velocities.as_mut());
+            update_positions(points.as_mut(), &velocities);
+            index += 1;
+
+            if initial_points.iter().map(dimension_function).collect::<Vec<i32>>() == points.iter().map(dimension_function).collect::<Vec<i32>>() {
+                break
+            }
+        }
+        index
+    }).collect();
+
+    println!("{:?}", results);
+
+    println!("{}", gcd(results[0], results[2]));
+    println!("{}", gcd(results[1], results[2]));
+    println!("{}", gcd(results[1], results[0]));
+
+    let final_result = results[0] * results[1] * results[2];
+
+    for i in &results {
+        println!("{} {}", i, 332477126821644.0 / *i as f32);
+    }
+
+    for i in &results {
+        println!("{} {}", i, 83119281705411.0 / *i as f32);
+    }
+
+    return Answer { question: input.question, result: final_result };
 }
 
-
+fn gcd(left: usize, right: usize) -> usize {
+    if left > right {
+        gcd(right, left)
+    } else {
+        let remain = right % left;
+        if remain == 0 {
+            left
+        } else {
+            gcd(left, remain)
+        }
+    }
+}
