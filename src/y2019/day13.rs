c2151b2110fd193324691cc419a9b8b9795d1d51
Yuchen Zhong <yzhong.cs@gmail.com>
2019-12-14 22:14:58 -0500

    WIP: game

--- src/y2019/day13.rs
+++ src/y2019/day13.rs
@@ -2,6 +2,8 @@ use super::super::helpers::parser::*;
 use super::day9::*;
 use std::collections::{HashMap, VecDeque};
 use crate::helpers::models::BigPoint;
+use std::thread::sleep;
+use std::time::Duration;
 
 // 0 is an empty tile. No game object appears in this tile.
 const EMPTY_TILE: i128 = 0;
@@ -15,7 +17,7 @@ const HORIZONTAL_PADDLE_TILE: i128 = 3;
 const BALL_TILE: i128 = 4;
 
 
-fn play_game(input: &Vec<i128>) -> usize {
+fn play_game(input: &Vec<i128>) -> HashMap<BigPoint, i128> {
     let mut map: HashMap<BigPoint, i128> = HashMap::new();
 
     let mut robot = SuperIntCodeComputer {
@@ -45,14 +47,70 @@ fn play_game(input: &Vec<i128>) -> usize {
         map.insert(BigPoint { x, y }, tile_id);
     }
 
-    map.values().filter(|x| **x == BLOCK_TILE).count()
+    map
 }
 
 pub fn part1(input: Input<Vec<i128>>) -> Answer<usize> {
-    let r = play_game(&input.data);
+    let map = play_game(&input.data);
+    let r = map.values().filter(|x| **x == BLOCK_TILE).count();
     Answer { question: input.question, result: r }
 }
 
 pub fn part2(input: Input<Vec<i128>>) -> Answer<String> {
+
+    // Use part 1 to just get the size of the scren
+    let map = play_game(&input.data);
+    let max_x = map.keys().into_iter().map(|p| p.x).max().unwrap();
+    let max_y = map.keys().into_iter().map(|p| p.y).max().unwrap();
+
+    let mut screen = vec![vec![' '; max_x as usize + 1]; max_y as usize + 1];
+
+    let mut robot = SuperIntCodeComputer {
+        numbers: input.data.clone(),
+        index: 0,
+        input_queue: VecDeque::new(),
+        relative_base: 0,
+        external_numbers: HashMap::new(),
+    };
+    loop {
+        let x = match robot.run() {
+            SuperIntCodeResult::Output(val) => val,
+            SuperIntCodeResult::Halted => break,
+        };
+
+        let y = match robot.run() {
+            SuperIntCodeResult::Output(val) => val,
+            SuperIntCodeResult::Halted => unimplemented!(),
+        };
+
+        let tile_id = match robot.run() {
+            SuperIntCodeResult::Output(val) => val,
+            SuperIntCodeResult::Halted => unimplemented!(),
+        };
+
+        screen[y as usize][x as usize] = match tile_id {
+            EMPTY_TILE => ' ',
+            WALL_TILE => '#',
+            BLOCK_TILE => 'x',
+            HORIZONTAL_PADDLE_TILE => '=',
+            BALL_TILE => 'O',
+            _ => unimplemented!()
+        };
+
+        let mut output: String = String::new();
+        for row in &screen {
+            for c in row {
+                output.push(c.clone());
+            }
+            output.push('\n');
+        }
+        println!("{}", output);
+        println!();
+        println!("-->");
+
+        sleep(Duration::from_millis(200));
+    }
+
+
     Answer { question: input.question, result: "1".to_string() }
 }
