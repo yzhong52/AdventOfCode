781bb4eac140152dda5787e1375a461ec5939d91
Yuchen Zhong <yzhong.cs@gmail.com>
2019-12-14 22:50:24 -0500

    WIP

--- src/y2019/day13.rs
+++ src/y2019/day13.rs
@@ -56,61 +56,84 @@ pub fn part1(input: Input<Vec<i128>>) -> Answer<usize> {
     Answer { question: input.question, result: r }
 }
 
+struct ArcadeCabinet {
+    computer: SuperIntCodeComputer,
+    screen: Vec<Vec<char>>,
+    score: i128,
+}
+
+impl ArcadeCabinet {
+    fn insert_coin(&mut self) {
+        self.computer.numbers[0] = 2;
+    }
+
+    fn start(&mut self) {
+        loop {
+            let x = match self.computer.run() {
+                SuperIntCodeResult::Output(val) => val,
+                SuperIntCodeResult::Halted => break,
+            };
+
+            let y = match self.computer.run() {
+                SuperIntCodeResult::Output(val) => val,
+                SuperIntCodeResult::Halted => unimplemented!(),
+            };
+
+            let tile_id = match self.computer.run() {
+                SuperIntCodeResult::Output(val) => val,
+                SuperIntCodeResult::Halted => unimplemented!(),
+            };
+
+            if x >= 0 {
+                self.screen[y as usize][x as usize] = match tile_id {
+                    EMPTY_TILE => ' ',
+                    WALL_TILE => '#',
+                    BLOCK_TILE => 'x',
+                    HORIZONTAL_PADDLE_TILE => '=',
+                    BALL_TILE => 'O',
+                    _ => unimplemented!()
+                };
+            } else {
+                self.score = tile_id;
+            }
+
+            let mut output: String = String::new();
+            for row in &self.screen {
+                for c in row {
+                    output.push(c.clone());
+                }
+                output.push('\n');
+            }
+            println!("{}", output);
+            println!("Current score: {}", self.score);
+            println!("-->");
+
+            sleep(Duration::from_millis(10));
+        }
+    }
+}
+
 pub fn part2(input: Input<Vec<i128>>) -> Answer<String> {
 
-    // Use part 1 to just get the size of the scren
+    // Use part 1 to just get the size of the screen
     let map = play_game(&input.data);
     let max_x = map.keys().into_iter().map(|p| p.x).max().unwrap();
     let max_y = map.keys().into_iter().map(|p| p.y).max().unwrap();
 
-    let mut screen = vec![vec![' '; max_x as usize + 1]; max_y as usize + 1];
+    let mut screen: Vec<Vec<char>> = vec![vec!['?'; max_x as usize + 1]; max_y as usize + 1];
 
-    let mut robot = SuperIntCodeComputer {
+    let mut computer = SuperIntCodeComputer {
         numbers: input.data.clone(),
         index: 0,
         input_queue: VecDeque::new(),
         relative_base: 0,
         external_numbers: HashMap::new(),
     };
-    loop {
-        let x = match robot.run() {
-            SuperIntCodeResult::Output(val) => val,
-            SuperIntCodeResult::Halted => break,
-        };
 
-        let y = match robot.run() {
-            SuperIntCodeResult::Output(val) => val,
-            SuperIntCodeResult::Halted => unimplemented!(),
-        };
+    let mut arcade = ArcadeCabinet { computer, screen, score: 0 };
 
-        let tile_id = match robot.run() {
-            SuperIntCodeResult::Output(val) => val,
-            SuperIntCodeResult::Halted => unimplemented!(),
-        };
-
-        screen[y as usize][x as usize] = match tile_id {
-            EMPTY_TILE => ' ',
-            WALL_TILE => '#',
-            BLOCK_TILE => 'x',
-            HORIZONTAL_PADDLE_TILE => '=',
-            BALL_TILE => 'O',
-            _ => unimplemented!()
-        };
-
-        let mut output: String = String::new();
-        for row in &screen {
-            for c in row {
-                output.push(c.clone());
-            }
-            output.push('\n');
-        }
-        println!("{}", output);
-        println!();
-        println!("-->");
-
-        sleep(Duration::from_millis(200));
-    }
-
+    arcade.insert_coin();
+    arcade.start();
 
     Answer { question: input.question, result: "1".to_string() }
 }
