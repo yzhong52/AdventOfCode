6a736ea36fa115829c630c225a3616d1671ae2f8
Yuchen Zhong <yzhong.cs@gmail.com>
2019-12-22 10:04:10 -0500

    portals finish

--- src/y2019/day20.rs
+++ src/y2019/day20.rs
@@ -1,57 +1,64 @@
 use super::super::helpers::parser::*;
 use crate::helpers::models::_Point;
+use std::collections::HashMap;
 
-pub trait Letter {
-    fn is_uppercase(&self) -> bool;
-}
-
-impl Letter for char {
-    fn is_uppercase(&self) -> bool {
-        return *self >= 'A' && *self <= 'Z';
-    }
-}
-
-fn search_portal(maze: &Vec<Vec<char>>) {
+fn search_portal(maze: &Vec<Vec<char>>) -> HashMap<[char; 2], Vec<_Point<i32>>> {
+    let mut portals: HashMap<[char; 2], Vec<_Point<i32>>> = HashMap::new();
     let max_x = maze.len();
     let max_y = maze[0].len();
     for i in 0..max_x {
         for j in 0..max_y {
             if maze[i][j].is_ascii_uppercase() {
                 let p1 = _Point { x: i, y: j };
-                let neighbours = p1.neighbours4(max_x, max_y);
+
+                let neighbours = [
+                    _Point { x: i + 1, y: j },
+                    _Point { x: i, y: j + 1 }
+                ];
+
                 let p2 = neighbours
                     .iter()
-                    .filter(|pos| maze[pos.x][pos.y].is_ascii_uppercase())
-                    .nth(0)
-                    .unwrap();
-
-                let side1 = _Point {
-                    x: p1.x as i32 + p1.x as i32 - p2.x as i32,
-                    y: p1.y as i32 + p1.y as i32 - p2.y as i32,
-                };
-
-                let side2 = _Point {
-                    x: p2.x as i32 + p2.x as i32 - p1.x as i32,
-                    y: p2.y as i32 + p2.y as i32 - p1.y as i32,
-                };
-
-                let both_sides = [side1, side2];
-
-                let portal_pos = both_sides.iter()
-                    .filter(|p| {
-                        p.is_valid(max_x as i32, max_y as i32) && maze[p.x as usize][p.y as usize] == '.'
+                    .filter(|pos| {
+                        pos.is_valid(max_x, max_y) && maze[pos.x][pos.y].is_ascii_uppercase()
                     })
-                    .nth(0)
-                    .unwrap();
+                    .nth(0);
 
-                println!("{}{}: {:?}", maze[p1.x][p1.y], maze[p2.x][p2.y], portal_pos);
+                if let Some(p2) = p2 {
+                    let side1 = _Point {
+                        x: p1.x as i32 + p1.x as i32 - p2.x as i32,
+                        y: p1.y as i32 + p1.y as i32 - p2.y as i32,
+                    };
+
+                    let side2 = _Point {
+                        x: p2.x as i32 + p2.x as i32 - p1.x as i32,
+                        y: p2.y as i32 + p2.y as i32 - p1.y as i32,
+                    };
+
+                    let both_sides = [side1, side2];
+
+                    let portal_pos = both_sides.iter()
+                        .filter(|p| {
+                            p.is_valid(max_x as i32, max_y as i32) && maze[p.x as usize][p.y as usize] == '.'
+                        })
+                        .nth(0)
+                        .unwrap();
+
+
+                    let key = [maze[p1.x][p1.y], maze[p2.x][p2.y]];
+                    portals.entry(key).or_insert(vec![]).push(portal_pos.clone());
+                }
             }
         }
     }
+    portals
 }
 
 pub fn part1(input: Input<Vec<Vec<char>>>) -> Answer<usize> {
-    search_portal(&input.data);
+    let portals = search_portal(&input.data);
+
+    for (pk, pv) in portals {
+        println!("{:?}: {:?}", pk, pv);
+    }
     Answer { question: input.question, result: 0 }
 }
 
