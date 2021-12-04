3ca33b73e450296ffbfbc423067d0d9545615901
Yuchen Zhong <yuchen21@fb.com>
2021-12-04 11:39:06 -0500

    day 1

--- 2021/day1.ts
+++ 2021/day1.ts
@@ -1,2 +1,16 @@
-let message: string = 'Hello World';
-console.log(message);
+import * as fs from 'fs';
+let lines: string[]  = fs.readFileSync('day1', 'utf8').split('\n');
+let numbers: number[] = lines.map(line => parseInt(line));
+
+function count_increased(gap: number): number {
+    var count = 0;
+    for (var i = gap; i < numbers.length; i++) {
+        if (numbers[i] > numbers[i - gap]) {
+            count += 1
+        }
+    }
+    return count;
+}
+
+console.log("Part 1:", count_increased(1));
+console.log("Part 2:", count_increased(3));
