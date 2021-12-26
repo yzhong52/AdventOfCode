9f0096d00a5033003f725cbcaffbe0d8594996a9
Yuchen Zhong <yzhong.cs@gmail.com>
2021-12-26 18:12:25 -0500

    --- Day 18: Snailfish --- Part 1

--- 2021/day18.ts
+++ 2021/day18.ts
@@ -1,6 +1,6 @@
 import { assert } from "console";
 import { assertEqual } from "./utils/asserts";
-import { readStrings } from "./helpers";
+import { print_result, readStrings } from "./helpers";
 
 class Pair {
     constructor(
@@ -11,6 +11,10 @@ class Pair {
     toString() {
         return `[${this.left.toString()},${this.right.toString()}]`
     }
+
+    clone() {
+        return new Pair(this.left.clone(), this.right.clone())
+    }
 }
 
 class Node {
@@ -18,16 +22,22 @@ class Node {
     constructor(public value: number | Pair) {
     }
 
-    static createPair(left: Node, right: Node) {
+    static createPair(left: Node, right: Node): Node {
         return new Node(new Pair(left, right))
     }
 
-    get left() {
-        return (this.value as Pair)?.left
+    getValue(): number | undefined {
+        if (typeof this.value === 'number') {
+            return this.value as number
+        }
+        return undefined
     }
 
-    get right() {
-        return (this.value as Pair)?.right
+    getChildren(): Pair | undefined {
+        if (typeof this.value === 'number') {
+            return undefined
+        }
+        return this.value as Pair
     }
 
     static parse(line: string): Node {
@@ -56,42 +66,46 @@ class Node {
 
     tryExplode() {
         var exploded = false
-        var previousNode: Node | undefined = undefined
-        var rightNumber: number | undefined = undefined
+        var previousLeafNode: Node | undefined = undefined
+        var pendingRightNumber: number | undefined = undefined
 
         function visit(depth: number, currentNode: Node) {
             if (typeof currentNode.value === 'number') {
-                previousNode = currentNode
-                if (rightNumber) {
-                    currentNode.value = rightNumber + (currentNode.value as number)
-                    rightNumber = undefined
+                previousLeafNode = currentNode
+                if (pendingRightNumber) {
+                    currentNode.value = pendingRightNumber + currentNode.getValue()!
+                    pendingRightNumber = undefined
                 }
-                return
             } else {
-                let value = currentNode.value as Pair
-                if (depth < 4) {
-                    visit(depth + 1, value.left)
-                    visit(depth + 1, value.right)
+                let children = currentNode.getChildren()!
+                if (depth < 4 || exploded) { // We only explode one number per round
+                    visit(depth + 1, children.left)
+                    visit(depth + 1, children.right)
                 } else {
-                    exploded = true
-                    assert(depth == 4)
-                    let leftNumber = (currentNode.left.value as number)
-                    if (rightNumber) {
-                        leftNumber += rightNumber
+                    let currentLeftNumber: number | undefined = children.left.getValue()
+                    let currentRightNumber: number | undefined = children.right.getValue()
+
+                    if (currentLeftNumber != undefined && currentRightNumber != undefined) {
+                        exploded = true
+                        console.log(`..Exploding pair ${currentLeftNumber}, ${currentRightNumber}`)
+                        if (previousLeafNode) {
+                            assert(typeof previousLeafNode.value === 'number')
+                            previousLeafNode.value = currentLeftNumber + previousLeafNode.getValue()!
+                        }
+                        pendingRightNumber = currentRightNumber
+                        previousLeafNode = currentNode
+                        currentNode.value = 0
+                    } else {
+                        visit(depth + 1, children.left)
+                        visit(depth + 1, children.right)
                     }
-                    rightNumber = (currentNode.right.value as number)
-                    if (previousNode) {
-                        previousNode.value = leftNumber + (previousNode.value as number)
-                    }
-                    previousNode = currentNode
-                    currentNode.value = 0
                 }
             }
         }
         visit(0, this)
 
         if (exploded) {
-            console.log("e", this.toString())
+            console.log("E", this.toString())
         }
         return exploded
     }
@@ -104,23 +118,29 @@ class Node {
     trySplit() {
         var hasSplit = false
         function visit(node: Node) {
-            if (typeof node.value === 'number') {
-                if (node.value >= 10) {
+            if (hasSplit) {
+                // Only split one node at a time
+                return
+            }
+            if (node.getValue() != undefined) {
+                let value = node.getValue()!
+                if (value >= 10) {
+                    console.log("..Splitting number", value)
                     hasSplit = true
                     node.value = new Pair(
-                        new Node(Math.floor(node.value / 2)),
-                        new Node(Math.ceil(node.value / 2))
+                        new Node(Math.floor(value / 2)),
+                        new Node(Math.ceil(value / 2))
                     )
                 }
             } else {
-                visit(node.left)
-                visit(node.right)
+                let child = node.getChildren()!
+                visit(child.left)
+                visit(child.right)
             }
         }
         visit(this)
-
         if (hasSplit) {
-            console.log("s", this.toString())
+            console.log("S", this.toString())
         }
         return hasSplit
     }
@@ -130,23 +150,50 @@ class Node {
         return this
     }
 
-    add(node: Node) {
+    add(node: Node): Node {
         console.log(" ", this.toString())
         console.log("+", node.toString())
-        let result = new Node(new Pair(this, node))
+        let result = new Node(new Pair(this.clone(), node.clone()))
         console.log("=", result.toString(), "\n")
         return result
     }
 
-    reduce() {
-        while (this.tryExplode() || this.trySplit()) {
-
+    clone(): Node {
+        if (this.getValue() != undefined) {
+            return new Node(this.getValue()!)
+        } else {
+            let children = this.getChildren()!
+            return new Node(children.clone())
         }
+    }
+
+    reduce(): Node {
+        console.log('b', this.toString())
+        while (true) {
+            // During reduction, at most one action applies, after which the process returns
+            // to the top of the list of actions. For example, if split produces a pair that
+            // meets the explode criteria, that pair explodes before other splits occur.
+            if (this.tryExplode()) {
+                continue
+            }
+            if (this.trySplit()) {
+                continue
+            }
+            break
+        }
+        console.log("R", this.toString())
         return this
     }
 
-    static sum(rows: Array<string>): Node {
-        let nodes = rows.map(row => Node.parse(row))
+    magnitude(): number {
+        if (this.getValue() != undefined) {
+            return this.getValue()!
+        } else {
+            return 3 * this.getChildren()?.left.magnitude()! + 2 * this.getChildren()?.right.magnitude()!
+        }
+    }
+
+    static sum(nodes: Array<Node>): Node {
         return nodes
             .slice(1)
             .reduce((previous, current) => previous.add(current).reduce(), nodes[0])
@@ -170,12 +217,12 @@ assertEqual(
 )
 
 assertEqual(
-    Node.parse('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]').explode().toString(),
+    Node.parse('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]').reduce().toString(),
     "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
 )
 
 assertEqual(
-    Node.parse('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]').explode().split().explode().toString(),
+    Node.parse('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]').reduce().toString(),
     "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
 )
 
@@ -187,8 +234,16 @@ let test1_to_6 = `[1,1]
 [6,6]
 `.trim().split('\n')
 assertEqual(
-    Node.sum(test1_to_6).toString(),
+    Node.sum(test1_to_6.map(row => Node.parse(row))).toString(),
     '[[[[5,0],[7,4]],[5,5]],[6,6]]'
+)
+
+
+assertEqual(
+    Node.parse('[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]')
+        .add(Node.parse('[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]'))
+        .reduce().toString(),
+    "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"
 )
 
 let test_larger = `[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
@@ -203,15 +258,21 @@ let test_larger = `[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
 [[[[4,2],2],6],[8,7]]
 `.trim().split('\n')
 assertEqual(
-    Node.sum(test_larger).toString(),
+    Node.sum(test_larger.map(row => Node.parse(row))).toString(),
     '[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]'
 )
 
 
-// let testNodeLeft = Node.parse('[[[[4,3],4],4],[7,[[8,4],9]]]')
-// let testNodeRight = Node.parse('[1,1]')
-// let node = new Node(new Pair(testNodeLeft, testNodeRight))
-// console.log("after addition:", node.toString())
-// node.explode()
-// console.log("after explode:", node.toString())
+let rows = readStrings(18)
+let nodes = rows.map(row => Node.parse(row))
+print_result(18, 1, Node.sum(nodes).magnitude())
 
+
+let part2 = 0
+for (let i = 0; i < nodes.length - 1; i++) {
+    for (let j = 1; j < nodes.length; j++) {
+        let magnitude = nodes[i].add(nodes[j]).magnitude()
+        part2 = Math.max(part2, magnitude)
+    }
+}
+print_result(18, 2, part2)
