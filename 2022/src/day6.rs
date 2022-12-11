dd493dfda77a882a33f308366066d5d7e891acc8
Yuchen Zhong <yzhong.cs@gmail.com>
2022-12-11 08:41:42 -0500

    day6

---
+++ 2022/src/day6.rs
@@ -0,0 +1,51 @@
+use itertools::Itertools;
+use std::{collections::HashSet, fs};
+
+pub fn day6() -> (String, String) {
+    let content = fs::read_to_string("input/day6").unwrap();
+    run(content)
+}
+
+fn run(content: String) -> (String, String) {
+    // remove any trailing '\n'
+    let content = content.trim();
+
+    let part1 = find_distinct(content, 4);
+    let part2 = find_distinct(content, 14);
+
+    println!("Day 4 part 1: {:?}", part1);
+    println!("Day 4 part 2: {:?}", part2);
+
+    (part1.to_string(), part2.to_string())
+}
+
+fn find_distinct(content: &str, distinct_count: usize) -> usize {
+    (distinct_count..content.len())
+        .find_or_first(|idx| {
+            let last_4 = &content.as_bytes()[*idx - distinct_count..*idx];
+            let set: HashSet<_> = HashSet::from_iter(last_4);
+            set.len() == distinct_count
+        })
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day6_test() {
+        let (part1, part2) = day6();
+        assert_eq!(part1, "1582");
+        assert_eq!(part2, "3588");
+    }
+
+    #[test]
+    fn day6_example_test() {
+        let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string();
+
+        let (part1, part2) = run(input);
+        assert_eq!(part1, "11");
+        assert_eq!(part2, "26");
+    }
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -3,12 +3,14 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 use crate::day1::day1;
 use crate::day2::day2;
 use crate::day3::day3;
 use crate::day4::day4;
 use crate::day5::day5;
+use crate::day6::day6;
 
 fn main() {
     day1();
@@ -16,4 +18,5 @@ fn main() {
     day3();
     day4();
     day5();
+    day6();
 }
