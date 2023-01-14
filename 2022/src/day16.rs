679a3b3a6955487bec67770ba22eb8b58449a1ec
Yuchen Zhong <yzhong.cs@gmail.com>
2023-01-14 14:17:55 -0500

    part 1

--- 2022/src/day16.rs
+++ 2022/src/day16.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::{
@@ -25,18 +26,23 @@ struct Pressure {
     total: i32,
 }
 
+type ValveName = String;
+
 struct ValveProp {
     rate: i32,
-    leading_valves: Vec<String>,
+    leading_valves: Vec<ValveName>,
 }
 
 fn run(content: String) -> (String, String) {
     lazy_static! {
+        // Examples
+        // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        // Valve HH has flow rate=22; tunnel leads to valve GG
         static ref RE: Regex =
             Regex::new(r"^Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.*)$")
                 .unwrap();
     }
-    let parsed: HashMap<String, ValveProp> = content
+    let parsed: HashMap<ValveName, ValveProp> = content
         .trim()
         .split("\n")
         .map(|line| -> (String, ValveProp) {
@@ -44,9 +50,9 @@ fn run(content: String) -> (String, String) {
                 .captures(line.trim())
                 .expect(&format!("Unable to unwrap string '{}'.", line.trim()));
 
-            let source = capture.get(1).unwrap().as_str().to_owned();
+            let source: ValveName = capture.get(1).unwrap().as_str().to_owned();
             let rate: i32 = capture.get(2).unwrap().as_str().parse::<i32>().unwrap();
-            let leading_valves = capture
+            let leading_valves: Vec<ValveName> = capture
                 .get(3)
                 .unwrap()
                 .as_str()
@@ -54,6 +60,7 @@ fn run(content: String) -> (String, String) {
                 .split(", ")
                 .map(|value| value.to_string())
                 .collect();
+
             let value_prop = ValveProp {
                 rate,
                 leading_valves,
@@ -77,6 +84,18 @@ fn run(content: String) -> (String, String) {
         },
     )];
 
+    // To keep track of the states, we only need to know
+    //
+    // 1) where we are currently located, `current_valve`
+    // 2) what the valves that have been opened, `opened_valves`
+    //
+    // Every step, we can take two actions,
+    //
+    // 1) open the current valve, which will add one more entry to `opened_valves`
+    // 2) move to another vale, which will change the value of `opened_valve`.
+    //
+    // Now, when we have multiple states to expand our search, which one should we start with
+    // so that we can find the optimal solution fastest?
     for _steps in 1..=30 {
         let mut next_visiting_states = vec![];
 
@@ -86,25 +105,32 @@ fn run(content: String) -> (String, String) {
                 .opened_valves
                 .contains(&current_visiting_state.current_valve)
             {
-                let mut new_state = current_visiting_state.clone();
-                new_state
-                    .opened_valves
-                    .insert(current_visiting_state.current_valve.clone());
-                let new_pressure = Pressure {
-                    releasing: pressure.releasing
-                        + parsed
+                let current_valve_property = parsed
+                    .get(&current_visiting_state.current_valve)
+                    .unwrap();
+
+                // Only consider opening the valve if the flow rate is greater than 0
+                if current_valve_property.rate > 0 {
+                    let mut new_state = current_visiting_state.clone();
+                    new_state
+                        .opened_valves
+                        .insert(current_visiting_state.current_valve.clone());
+                    let new_pressure = Pressure {
+                        releasing: pressure.releasing
+                            + parsed
                             .get(&current_visiting_state.current_valve)
                             .unwrap()
                             .rate,
-                    total: pressure.total + pressure.releasing,
-                };
-                if visited_states
-                    .get(&new_state)
-                    .map(|total_pressure| total_pressure < &new_pressure.total)
-                    .unwrap_or(true)
-                {
-                    visited_states.insert(new_state.clone(), new_pressure.total);
-                    next_visiting_states.push((new_state, new_pressure));
+                        total: pressure.total + pressure.releasing,
+                    };
+                    if visited_states
+                        .get(&new_state)
+                        .map(|total_pressure| total_pressure < &new_pressure.total)
+                        .unwrap_or(true)
+                    {
+                        visited_states.insert(new_state.clone(), new_pressure.total);
+                        next_visiting_states.push((new_state, new_pressure));
+                    }
                 }
             }
 
@@ -136,9 +162,43 @@ fn run(content: String) -> (String, String) {
             visited_states.insert(current_visiting_state.clone(), new_pressure.total);
             next_visiting_states.push((current_visiting_state.clone(), new_pressure));
         }
-        visiting_states = next_visiting_states;
 
-        println!("steps {} {}", _steps, visited_states.len());
+        // Can we do some more pruning?
+        let next_visiting_states_len = next_visiting_states.len();
+
+        let steps_remained = 30 - _steps;
+
+        let overall_minimum_release_pressure = next_visiting_states
+            .iter()
+            .map(|(_state, pressure)| pressure.releasing * steps_remained + pressure.total)
+            .max()
+            .unwrap();
+
+        visiting_states = next_visiting_states
+            .into_iter()
+            .filter(|(state, pressure)| {
+                // From all the remaining valves, find the one with the maximum flow that is not yet
+                // opened.
+                let max_unopened_flow = parsed
+                    .iter()
+                    .filter(|(valve_name, _)| !state.opened_valves.contains(*valve_name))
+                    .map(|(_, valve_property)| valve_property.rate)
+                    .max()
+                    .unwrap_or(0);
+
+                let maximum_release_pressure = (max_unopened_flow * ((30 - _steps) / 2) + pressure.releasing) * steps_remained + pressure.total;
+
+                maximum_release_pressure >= overall_minimum_release_pressure
+            })
+            .collect_vec();
+
+
+        println!(
+            "steps: {}, potential states count: {}, states pruned: {}",
+            _steps,
+            next_visiting_states_len,
+            next_visiting_states_len as i32 - visiting_states.len() as i32,
+        );
     }
 
     let part1: i32 = visiting_states
@@ -180,7 +240,7 @@ mod tests {
     #[test]
     fn day16_test() {
         let (part1, part2) = day16();
-        assert_eq!(part1, "xx");
+        assert_eq!(part1, "2181");
         assert_eq!(part2, "xx");
     }
 }
